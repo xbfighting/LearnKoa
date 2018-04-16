cb33fbdf310114853e1c1d8352dd50a2b13e045b
baileicheng <xbfighting@hotmail.com>
2018-04-16 11:03:05 +0800

    change console to print processing

--- my-koa-app.js
+++ my-koa-app.js
@@ -8,6 +8,7 @@ app.use(async (ctx, next) => {
 	await next();
 	const ms = Date.now() - start;
 	ctx.set(`X-Response-Time ${ms}ms`);
+	console.log(`X-Response-Time - ${ms}ms`);
 });
 
 // logger
@@ -15,13 +16,14 @@ app.use(async (ctx, next) => {
 	const start = Date.now();
 	await next();
 	const ms = Date.now() - start;
-	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+	console.log(`logger - ${ms}ms`);
 });
 
 // response
 
 app.use(async ctx => {
 	ctx.body = 'Hello World';
+	console.log(`${ctx.method} ${ctx.url} - Hello World`);
 });
 
 
