40c6ec69595775c6dff045d6cc37664bc6597644
baileicheng <xbfighting@hotmail.com>
2018-04-16 10:55:08 +0800

    add my-koa-app.js

---
+++ my-koa-app.js
@@ -0,0 +1,28 @@
+"use strict"
+const Koa = require("koa");
+const app = new Koa();
+
+// x-response-time
+app.use(async (ctx, next) => {
+	const start = Date.now();
+	await next();
+	const ms = Date.now() - start;
+	ctx.set(`X-Response-Time ${ms}ms`);
+});
+
+// logger
+app.use(async (ctx, next) => {
+	const start = Date.now();
+	await next();
+	const ms = Date.now() - start;
+	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+	ctx.body = 'Hello World';
+});
+
+
+app.listen(8020, () => { console.log('goto http://localhost:8020') });
